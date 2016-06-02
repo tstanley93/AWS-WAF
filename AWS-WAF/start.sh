ad42bb284d2435aa4306d3d3b43d03ff6582be2f
Thomas Stanley <t.stanley@f5.com>
2016-06-02 12:48:37 -0700

    added readme with button ...

--- AWS-WAF/start.sh
+++ AWS-WAF/start.sh
@@ -8,7 +8,7 @@
 ## fff       f  f5555555             Written By: EIS Consulting          ##
 ## f        ff  f5555555                                                 ##
 ## fff   ffff       f555             Date Created: 12/02/2015            ##
-## fff    fff5555    555             Last Updated: 01/21/2016            ##
+## fff    fff5555    555             Last Updated: 06/02/2016            ##
 ##  ff    fff 55555  55                                                  ##
 ##   f    fff  555   5       This script will start the pre-configured   ##
 ##   f    fff       55       WAF configuration.                          ##
@@ -21,6 +21,7 @@
 ## Version #     Name       #                    NOTES                   ##
 ###########################################################################
 ## 11/23/15#  Thomas Stanley#    Created base functionality              ##
+## 06/02/16#  Thomas Stanley#    FInished base functionality             ##
 ###########################################################################
 
 ### Parameter Legend  ###
